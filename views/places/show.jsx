055943c6fe007f8c2ae7d6b421f48d925d720097
Daniel Flores <your@example.com>
2023-11-30 17:26:30 -0800

    ability to delete places

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -18,6 +18,12 @@ function show (data) {
               <h2>comments</h2>
               <p>no comments yet</p>
             </section>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit </a>     
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+            <button type="submit" className="btn btn-danger"> Delete </button>
+            </form> 
+    
+
           </main>
         </Def>
     )
